6c61260af74cdf69d0d6e2df6f8dfb491bfe72d6
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-01-19 20:03:25 -0300

    Adding validation on getTaskFilterDTO

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,11 @@
 import { TaskStatus } from '../task.model';
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   readonly status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   readonly search: string;
 }
