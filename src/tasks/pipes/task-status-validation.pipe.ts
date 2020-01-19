773d76fc2c3e9ab1a4b3dc4003186acf76fae708
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-01-19 19:58:34 -0300

    Creating a custom validation task

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE
+  ];
+
+  transform(value: string) {
+    const formattedValue = value.toUpperCase();
+
+    if (!this.isStatusValid(formattedValue)) {
+      throw new BadRequestException(`${formattedValue} is an invalid status`);
+    }
+
+    return formattedValue;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
